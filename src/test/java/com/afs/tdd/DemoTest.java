d73db42f995a35fa7fb0a24649623590cd306618
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 17:21:32 +0800

    [TTD practice 2][Marcus]feat:  testcase move_W.

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -153,4 +153,17 @@ class DemoTest {
         assertEquals("0 -1 S", result);
     }
 
+    @Test
+    void should_coordinateX_change_to_minus_1_when_move_Mars_Rover_given_direction_W_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        String command = "M";
+
+        //when
+        String result = marsRover.controlMarsRover(command);
+
+        //return
+        assertEquals("-1 0 W", result);
+    }
+
 }
