e0ff6476730b7258701774961c96bdd7ea1c7dab
Marcus Ho <marcus.ho@oocl.com>
2021-12-02 17:10:44 +0800

    [TTD practice 2][Marcus]feat:  testcase direct_change_to_E.

---
+++ src/main/java/com/afs/tdd/Objects/MarsRover.java
@@ -0,0 +1,17 @@
+package com.afs.tdd.Objects;
+
+public class MarsRover {
+    private int coordinateX;
+    private int coordinateY;
+    private String direction;
+
+    public MarsRover(int coordinateX, int coordinateY, String direction) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.direction = direction;
+    }
+
+    public String controlMarsRover(String command) {
+        return null;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,21 @@
 package com.afs.tdd;
 
+import com.afs.tdd.Objects.MarsRover;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_direct_change_to_E_when_rotate_Mars_Rover_given_direction_N_and_command_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        String command = "R";
 
+        //when
+        String result = marsRover.controlMarsRover(command);
+
+        //return
+        assertEquals("0 0 E", result);
     }
 }
